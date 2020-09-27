af02b8445aa92b94e8dfafcd76a0746573e49fa6
Kaj Białas <kaj.bialas@dev-industry.com>
2020-09-27 16:31:13 +0200

    commti

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './routes/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Subpage from './routes/Subpage';
+import Nav from './components/Nav';
+import Todo from './routes/Todo';
+import configureStore from './store';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/subpage" component={Subpage} />
+        <Route exact path="/todo" component={Todo} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
