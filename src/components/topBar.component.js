a23e8891264d04763309138ccfa347d6fe7f5f01
Kaj Białas <kaj.bialas@dev-industry.com>
2020-09-27 14:05:56 +0200

    Next tests for react

--- src/components/topBar.component.js
+++ src/components/topBar.component.js
@@ -8,4 +8,8 @@ function TopBar({text}) {
   )
 }
 
+TopBar.defaultProps = {
+  text: 'Domyślny nagłówek',
+}
+
 export default TopBar;
--- src/components/topBar.component.test.js
+++ src/components/topBar.component.test.js
@@ -3,17 +3,27 @@ import { render } from '@testing-library/react';
 import TopBar from './topBar.component';
 
 describe('topBar.component.js', () => {
-  it('should render correctly text', () => {
+  it('should render correctly text if prop text isnt derived', () => {
+    const { getByTestId } = render(<TopBar />);
+    const result = getByTestId('topBarContainer');
+    const expectedResult = 'Domyślny nagłówek';
+
+    expect(result).toHaveTextContent(expectedResult);
+  });
+
+  it('should render correctly text if prop text is derived', () => {
     const { getByTestId } = render(<TopBar text="Example text" />);
     const result = getByTestId('topBarContainer');
+    const expectedResult = 'Example text';
 
-    expect(result).toHaveTextContent("Example text");
+    expect(result).toHaveTextContent(expectedResult);
   });
 
   it('should have correct class css', () => {
     const { getByTestId } = render(<TopBar text="Example text" />);
     const result = getByTestId('topBarContainer');
+    const expectedResult = 'topBar';
 
-    expect(result).toHaveClass('topBar');
+    expect(result).toHaveClass(expectedResult);
   });
 });
